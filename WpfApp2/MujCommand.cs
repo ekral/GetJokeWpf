81b17f3538a3782e2a85cbcf3daac0e9f186f404
Erik Král <ekral@fai.utb.cz>
2018-11-29 12:47:37 +0100

    Add project files.

---
+++ WpfApp2/MujCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp2
+{
+    public class MujCommand : ICommand
+    {
+        private Action<object> _execute;
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            System.Diagnostics.Debug.WriteLine("Execute start");
+
+            _execute(parameter);
+
+            System.Diagnostics.Debug.WriteLine("Execute Joke end");
+
+        }
+
+        public MujCommand(Action<object> execute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        }
+    }
+}
---
+++ WpfApp2/ViewModel.cs
@@ -0,0 +1,61 @@
+﻿
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    public class Value
+    {
+        
+        public int id { get; set; }
+        public string joke { get; set; }
+        public List<string> categories { get; set; }
+    }
+
+    public class RootObject
+    {
+        public string type { get; set; }
+        public Value value { get; set; }
+    }
+
+    public class ViewModel : ViewModelBase
+    {
+        private string _joke;
+
+        public string Joke
+        {
+            get { return _joke; }
+            set { _joke = value; OnPropertyChanged(); }
+        }
+
+
+        public MujCommand CommandGetJoke { get; set; }
+
+        public ViewModel()
+        {
+            CommandGetJoke = new MujCommand(async (param) => await GetJoke());
+        }
+
+        public async Task GetJoke()
+        {
+            System.Diagnostics.Debug.WriteLine("Get Joke start");
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync("https://api.icndb.com/jokes/random");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    JObject json = JObject.Parse(content);
+                    Joke = (string)json["value"]["joke"];
+                }
+            }
+
+            System.Diagnostics.Debug.WriteLine("Get Joke end");
+
+        }
+    }
+}
